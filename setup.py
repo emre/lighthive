004ed57482a16fcbaffb393c85dfb6ac9d9ef169
Emre Yilmaz <mail@emreyilmaz.me>
2022-10-10 14:07:30 +0200

    Version bump to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lighthive',
-    version='0.4.0a2',
+    version='0.4.0',
     packages=find_packages('.'),
     url='http://github.com/emre/lighthive',
     license='MIT',
